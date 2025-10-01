f29ef7fd031a077aa0ab027ef1bfd8227ee1cac3
luciano azzigotti <lucianoazzigotti@gmail.com>
2025-10-01 08:02:14 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
-// @ts-check
+// astro.config.mjs
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+// importamos el plugin que traduce los delimitadores personalizados
+import slugMathRemark from './src/plugins/slug-math-remark.js'
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://musiki.github.io/cymp/',
+  output: 'static',                   // build estático integrado
+  integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [
+      slugMathRemark, // primero traducimos $< >$ a $ $
+      remarkMath
+    ],
+    rehypePlugins: [rehypeKatex]
+  }
+})
+
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'CYM';
+export const SITE_DESCRIPTIOlN = 'repositorio de información de la cátedra Ciencia y Música LicMus UNTREF!';
